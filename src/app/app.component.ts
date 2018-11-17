f8fd54d555078d2765e257536fe31e8caa9d22a1
Van Nam Pham <vapha15@student.sdu.dk>
2018-11-17 19:31:55 +0100

    Added server module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,48 @@ export class AppComponent {
   title = 'Titel: my-dream-app';
 
   ngAfterContentInit() {
-    d3.select('p').style('color', 'blue');
+   // d3.json("assets/buildings.json").then(function (data: any[]) {
+      d3.json("http://localhost:3000/api/market").then(function (data: any[]) {
+      console.log(data);
 
+      data.forEach(function (d) {
+        d.height = +d.height;
+      });
+      var svg = d3.select("#chart-area")
+        .append("svg")
+        .attr("width", "500")
+        .attr("height", "500");
+
+      var rects = svg.selectAll("rect")
+        .data(data)
+        .enter().append("rect")
+        .attr("y", 0)
+        .attr("x", function (d, i) {
+          return (i * 60);
+        })
+        .attr("width", 40)
+        .attr("height", function (d) {
+          return d.height;
+        })
+        .attr("fill", function(d) {
+          return "grey";
+      });
+
+       
+
+    })
+  }
+  clicked(event: any) {
+    d3.select(event.target)
+      .append('circle')
+      .attr('cx', event.x)
+      .attr('cy', event.y)
+      .attr('r', 10)
+      .attr('fill', 'green')
+    console.log("Green")
+  }
+
+  clicked2(event: any) {
     d3.json("assets/buildings.json").then(function (data: any[]) {
       console.log(data);
 
@@ -36,18 +76,9 @@ export class AppComponent {
           return d.height;
         })
         .attr("fill", function (d) {
-          return "grey";
+          return "yellow";
         });
 
     })
-  }
-  clicked(event: any) {
-    d3.select(event.target)
-      .append('circle')
-      .attr('cx', event.x)
-      .attr('cy', event.y)
-      .attr('r', 10)
-      .attr('fill', 'green')
-    console.log("Green")
   }
 }
